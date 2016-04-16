a86aa52139a5971d78da8a1ded69d3c7a9dc8167
Tej Chajed <tchajed@mit.edu>
2016-04-16 14:09:47 -0400

    Reorganize code into directories

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,26 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'react-hot',
-      },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
+  // https://webpack.github.io/docs/configuration.html
+  // eval modules (some performance trick?) and provide a SourceMap
+  devtools: 'eval-source-map',
+  entry: [
+    './src/main.js',
+  ],
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: 'bundle.js',
+    publicPath: '/build/',
   },
-};
+  plugins: [
+    // can't get new watching plugin to work
+    new webpack.OldWatchingPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src'),
+    }]
+  }
+}
