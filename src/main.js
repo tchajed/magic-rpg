4f276378756018b914d8978353eb3520b07a53de
Tej Chajed <tchajed@mit.edu>
2016-04-23 12:57:48 -0400

    Remove React/webpack prototype

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AsciiGrid from './components/ascii.jsx';
-
-// note: 150x50 is a large grid, more than that will not really even fit
-ReactDOM.render(<AsciiGrid width={15} height={5}/>, document.getElementById('ascii-grid'));
