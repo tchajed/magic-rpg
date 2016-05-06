57b4d7bbeefa07ca195e380108d5bb1ac472b543
Tej Chajed <tchajed@mit.edu>
2016-05-06 15:08:58 -0400

    Remove prototype scenery and objects

--- magic-rpg/src/assets/level1.js
+++ magic-rpg/src/assets/level1.js
@@ -3,67 +3,36 @@ import {Bounds} from '../game/graphics';
 import Entity from '../game/entity';
 
 export const background = Background.create(asciiBlock(`
-+--+xxx+--+
-|  |xxx|  |
-|  +---+  +----------------------------------------+xxxxxxx+--------------------------+
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|         B                |
-|                                                  |xxxxxxx|                          |
-|                                                  +-------+G                         |
-|                                                                                     |
-|                                                                                     |
-|                                                               E                     |
-|                                                  +-------+                          |
-|                                                  |xxxxxxx|                          |
-|                                        A         |xxxxxxx|        N                 |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-|                                                  |xxxxxxx|                          |
-+-----+     +--------------------------------------+xxxxxxx+--------------------------+
-xxxxxx|     |
-xxxxxx|     +-------------------------------------------------------------------------------------------+
-xxxxxx|                                                                                                 |
-xxxxxx|                                                                                                 |
-xxxxxx+------------------------------------------------------------------------------------------       |
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|       |
-+-----------------------------------------------+---------+-------+----------+--------+xxxxxxxxx|       |
-|                                               |      M  |       |          |        |xxxxxxxxx|       |
-|        |   |   |               |   |   |      |  ####   |       |          |        |xxxxxxxxx|       |
-|        |P  |   |               |   |   |      |  ####   |       |          |        |xxxxxxxxx|       |
-|     ---+---+---+---         ---+---+---+---   |         |       |          |        |xxxxxxxxx|       |
-|        |   |   |               |   |   |      |         |       |          |        |xxxxxxxxx|       |
-|        |   |   |               |   |   |      +-- ------+       +------- --+        |xxxxxxxxx|       |
-|                                                                                     |xxxxxxxxx|       |
-|                                                                                     |xxxxxxxxx|       |
-|        |   |   |               |   |   |                                            |xxxxxxxxx|       |
-|        |   |   |               |   |   |                                            +---------+       |
-|     ---+---+---+---         ---+---+---+---                                                           |
-|        |   |   |               |   |   |                                                              |
-|        |   |   |               |   |   |                                                              |
-|                                                                                     +-----------------+
-|                                                                                     |
-|       +--------------------  ---+                                                   |
-|       |                         |                                                   |
-|       |  T                      |                                                   |
-|       |                         |                                                   |
-|       |                         |                                                   |
-|       |                         |                                                   |
-|       |                         |                                                   |
-|       |                         |                                                   |
-+-------+-------------------------+---------------------------------------------------+
++-----------------------------------------------+---------+-------+----------+--------+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+|                                               |      M  |       |          |        |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+|        |   |   |               |   |   |      |  ####   |       |          |        |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+|        |P  |   |               |   |   |      |  ####   |       |          |        |xxxxxxxxx+------------------------------------------------------------------------------------------+
+|     ---+---+---+---         ---+---+---+---   |         |       |          |        |xxxxxxxxx|                                                                                          |
+|        |   |   |               |   |   |      |         |       |          |        |xxxxxxxxx|                                                                                          |
+|        |   |   |               |   |   |      +-- ------+       +------- --+        |xxxxxxxxx|                                                                                          |
+|                                                                                     |xxxxxxxxx|          /--\\                                                                            |
+|                                                                                     |xxxxxxxxx|         /####\\                                                                           |
+|        |   |   |               |   |   |                                            |xxxxxxxxx|         +----+                                                                           |
+|        |   |   |               |   |   |                                            +---------+         |    |                                                                           |
+|     ---+---+---+---         ---+---+---+---                                                             |    |                                                                           |
+|        |   |   |               |   |   |                                                                +----+                                                                           |
+|        |   |   |               |   |   |                                                                                                                                                 |
+|                                                                                     +---------+                                                                                          |
+|                                                                                     |xxxxxxxxx|                                                                                          |
+|       +--------------------  ---+                                                   |xxxxxxxxx|                                                                                          |
+|       |                         |                    N                              |xxxxxxxxx|                                                                                          |
+|       |  T                      |                                                   |xxxxxxxxx|                                                                                          |
+|       |                         |                                                   |xxxxxxxxx|                                                                                          |
+|       |                         |                                                   |xxxxxxxxx|                                                                                          |
+|       |                         |                                                   |xxxxxxxxx+------------------------------------------------------------------------------------------+
+|       |                         |                                                   |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+|       |                         |                                                   |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
++-------+-------------------------+---------------------------------------------------+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 `), {
   P: {name: 'player'},
   T: {name: 'table'},
   M: {name: 'manager'},
-  A: {name: 'bossA'},
-  B: {name: 'bossB'},
-  E: {name: 'enemy'},
   N: {name: 'news'},
-  G: {name: 'greeter'},
 }).boxDrawing();
 
 const player = new Entity(
@@ -87,30 +56,6 @@ const manager = new Entity(
   }
 );
 
-const greeter = new Entity(
-  "greeter",
-  background.loc('greeter'),
-  Texture.create("*")
-);
-
-const bossA = new Entity(
-  "Boss 1",
-  background.loc('bossA'),
-  Texture.create("A")
-);
-
-const bossB = new Entity(
-  "Final Boss",
-  background.loc('bossB'),
-  Texture.create("B")
-);
-
-const enemy = new Entity(
-  "Generic enemy",
-  background.loc('enemy'),
-  Texture.create("E")
-);
-
 const table = new Entity(
   "Table",
   background.loc('table'),
@@ -127,10 +72,6 @@ export const objects = {
   player,
   news,
   manager,
-  greeter,
-  bossA,
-  bossB,
-  enemy,
   table
 };
 
